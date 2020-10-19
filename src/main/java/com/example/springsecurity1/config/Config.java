4e655a2d9dd5a734d78b9dce1a67979f259453f0
adrianscorus14 <adrianscorus14@gmail.com>
2020-10-19 11:07:32 +0300

    learning spring security

--- src/main/java/com/example/springsecurity1/config/Config.java
+++ src/main/java/com/example/springsecurity1/config/Config.java
@@ -2,6 +2,8 @@ package com.example.springsecurity1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
@@ -15,6 +17,14 @@ public class Config {
     @Bean
     public UserDetailsService userDetailsService(){
         InMemoryUserDetailsManager manager=new InMemoryUserDetailsManager();
+
+        UserDetails user1= User.withUsername("Bill").password("1234").roles("ADMIN").authorities("write").build();
+        var user2=User.withUsername("John").password("1234").roles("MANAGER").authorities("read").build();
+
+        //i used plain text only for theoretically purpose
+        
+        manager.createUser(user1);
+        manager.createUser(user2);
 
         return manager;
     }
